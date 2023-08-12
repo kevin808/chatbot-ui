22c755f0339c03ab15e3712a3bdc47776db8742f
Kevin Li <lkevi@microsoft.com>
2023-08-12 09:36:37 +0800

    add gpt-35-turbo-16k

--- types/openai.ts
+++ types/openai.ts
@@ -10,13 +10,15 @@ export interface OpenAIModel {
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
   GPT_3_5_AZ = 'gpt-35-turbo',
+  GPT_3_5_16k_AZ = 'gpt-35-turbo-16k',
+  Text_Embedding_Ada_AZ = 'text-embedding-ada-002',
   Code_Davinci_002_AZ = 'code-davinci-002',
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4-32k',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.GPT_3_5_AZ;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_3_5]: {
@@ -31,11 +33,23 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     maxLength: 12000,
     tokenLimit: 4000,
   },
+  [OpenAIModelID.GPT_3_5_16k_AZ]: {
+    id: OpenAIModelID.GPT_3_5_16k_AZ,
+    name: 'GPT-3.5-16k',
+    maxLength: 48000,
+    tokenLimit: 16000,
+  },
   [OpenAIModelID.Code_Davinci_002_AZ]: {
     id: OpenAIModelID.Code_Davinci_002_AZ,
     name: 'Code-Davinci-002',
     maxLength: 12000,
     tokenLimit: 4000,
+  },
+  [OpenAIModelID.Text_Embedding_Ada_AZ]: {
+    id: OpenAIModelID.Text_Embedding_Ada_AZ,
+    name: 'Text_Embedding_Ada',
+    maxLength: 24000,
+    tokenLimit: 8000,
   },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
